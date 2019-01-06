599076551f53a3c5b2e53107f5b67db04441ed4c
Sebastien Klasa <klasa.sebastien@gmail.com>
2019-01-06 16:56:22 +0100

    Add acc_t

--- circle_detection.cpp
+++ circle_detection.cpp
@@ -6,6 +6,49 @@
 #include <chrono>
 #include <cmath>
 
+struct acc_t : public std::vector<std::vector<std::vector<double>>>
+{
+    int min_i; int max_i; int step_i;
+    int min_j; int max_j; int step_j;
+    int min_r; int max_r; int step_r;
+
+    int rows, cols, rays;
+
+    acc_t(
+        int min_i, int max_i, int step_i,
+        int min_j, int max_j, int step_j,
+        int min_r, int max_r, int step_r
+    ) : std::vector<std::vector<std::vector<double>>>(
+        (max_i - min_i) / step_i + 1,
+        std::vector<std::vector<double>>(
+            (max_j - min_j) / step_j + 1,
+            std::vector<double>(
+                (max_r - min_r) / step_r + 1,
+                0
+            )
+        )
+    ) {
+        this->min_i  = min_i;
+        this->max_i  = max_i;
+        this->step_i = step_i;
+        this->min_j  = min_j;
+        this->max_j  = max_j;
+        this->step_j = step_j;
+        this->min_r  = min_r;
+        this->max_r  = max_r;
+        this->step_r = step_r;
+
+        this->rows = (max_i - min_i) / step_i;
+        this->cols = (max_j - min_j) / step_j;
+        this->rays = (max_r - min_r) / step_r;
+    }
+
+    void inc(double i, double j, double r, double value)
+    {
+        this->operator[]((i - min_i) / step_i)[(j - min_j) / step_j][(r - min_r) / step_r] += value;
+    }
+};
+
 int main(int argc, char** argv)
 {
     arguments_t args(argc, argv);
@@ -31,57 +74,43 @@ int main(int argc, char** argv)
     cv::Mat edges;
     cv::addWeighted(absSobelX, 0.5, absSobelY, 0.5, 0, edges);
 
-    cv::imshow("edges", edges);
-
     // Threshold filter
     // double min, max;
     // cv::minMaxLoc(edges, &min, &max);
     // cv::threshold(edges, edges, 0.8 * max, 255, cv::ThresholdTypes::THRESH_BINARY);
 
     // Creating acc
-    std::vector<std::vector<std::vector<double>>> acc;
-    int r_max = edges.rows - 1;
-    int c_max = edges.cols - 1;
-    int rad_min = 5;
-    int rad_max = (int)(std::max(r_max, c_max) * std::sqrt(2));
-    for (int i = 0; i < r_max; i++)
-    {
-        std::vector<std::vector<double>> temp1;
-        for (int j = 0; j < c_max; j++)
-        {
-            std::vector<double> temp2;
-            for (int k = rad_min; k < rad_max; k++)
-                temp2.push_back(0);
-            temp1.push_back(temp2);
-        }
-        acc.push_back(temp1);
-    }
+    acc_t acc(
+        0, edges.rows - 1, 1,
+        0, edges.cols - 1, 1,
+        5, (int)(std::max(edges.rows - 1, edges.cols - 1) * std::sqrt(2)), 1
+    );
 
     // Incrementing acc
     auto start = std::chrono::high_resolution_clock::now();
     for (int x = 0; x < edges.cols; x++)
     {
         for (int y = 0; y < edges.rows; y++)
-            for (int i = 0; i < r_max; i++)
-                for (int j = 0; j < c_max; j++)
+            for (int i = acc.min_i; i < acc.max_i; i += acc.step_i)
+                for (int j = acc.min_j; j < acc.max_j; j += acc.step_j)
                 {
                     int distance = (int)std::sqrt((j-x)*(j-x)+(i-y)*(i-y));
-                    if (distance >= rad_min)
-                        acc[i][j][distance - rad_min] += edges.at<uchar>(x, y);
+                    if (distance >= acc.min_r)
+                        acc.inc(i, j, distance, edges.at<uchar>(x, y));
                 }
-        std::cout << "\rSearching circles... " << (int)((x+1) * 100 / edges.cols) << "%";
+        std::cout << "\rSearching circles... " << (int)((x+1) * 100 / edges.cols) << "%" << std::flush;
     }
     auto finish = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = finish - start;
     std::cout << " " << (int)(elapsed.count() * 1000) << " ms" << std::endl;
 
     // Normalizing acc
-    for (int k = rad_min; k < rad_max; k++)
+    for (int r = 0; r < acc.rays; r++)
     {
-        double circumference = 2 * 3.14 * k;
-        for (int i = 0; i < r_max; i++)
-            for (int j = 0; j < c_max; j++)
-                acc[i][j][k-rad_min] /= circumference;
+        double circumference = 2 * 3.14 * (r * acc.step_r + acc.min_r);
+        for (int i = 0; i < acc.rows; i++)
+            for (int j = 0; j < acc.cols; j++)
+                acc[i][j][r] /= circumference;
     }
     
     // Finding local maxima
@@ -91,59 +120,64 @@ int main(int argc, char** argv)
         maxValues[i] = 0;
     int d = 5;
     start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < r_max; i++)
+    for (int i = 0; i < acc.rows; i++)
     {
-        for (int j = 0; j < c_max; j++)
+        for (int j = 0; j < acc.cols; j++)
         {
-            for (int k = rad_min; k < rad_max; k++)
+            for (int r = 0; r < acc.rays; r++)
             {
-                double value = acc[i][j][k - rad_min];
+                double value = acc[i][j][r];
                 if (value > 0)
                 {
                     bool is_local_max = true;
-                    for (int di = std::max(-d, -i); di <= std::min(d, c_max - i - 1) && is_local_max; di++)
+                    for (int di = std::max(-d, -i); di <= std::min(d, acc.cols - i - 1) && is_local_max; di++)
                     {
-                        for (int dj = std::max(-d, -j); dj <= std::min(d, r_max - j - 1) && is_local_max; dj++)
+                        for (int dj = std::max(-d, -j); dj <= std::min(d, acc.rows - j - 1) && is_local_max; dj++)
                         {
-                            for (int dk = std::max(-d, -k+rad_min); dk <= std::min(d, rad_max-k-1-rad_min) && is_local_max; dk++)
+                            for (int dr = std::max(-d, -r); dr <= std::min(d, acc.max_r-r-1) && is_local_max; dr++)
                             {
-                                double v = acc[i+di][j+dj][k+dk-rad_min];
+                                double v = acc[i+di][j+dj][r+dr];
                                 if (value < v)
                                     is_local_max = false;
                                 else if (v < value)
-                                    acc[i+di][j+dj][k+dk-rad_min] = 0;
+                                    acc[i+di][j+dj][r+dr] = 0;
                             }
                         }
                     }
                     if (!is_local_max)
-                        acc[i][j][k-rad_min] = 0;
+                        acc[i][j][r] = 0;
                 }
             }
         }
-        std::cout << "\rSearching local maxima... " << (int)((i+1) * 100 / r_max) << "%" << std::flush;
+        std::cout << "\rSearching local maxima... " << (int)((i+1) * 100 / acc.rows) << "%" << std::flush;
     }
     finish = std::chrono::high_resolution_clock::now();
     elapsed = finish - start;
     std::cout << " " << (int)(elapsed.count() * 1000) << " ms" << std::endl;
     
     // Searching n greater values
-    for (int i = 0; i < r_max; i++)
+    for (int i = 0; i < acc.rows; i++)
     {
-        for (int j = 0; j < c_max; j++)
+        for (int j = 0; j < acc.cols; j++)
         {
-            for (int k = rad_min; k < rad_max; k++)
+            for (int r = 0; r < acc.rays; r++)
             {
                 for (int l = 0; l < maxValues.size(); l++)
                 {
-                    if (maxValues[l] < acc[i][j][k-rad_min])
+                    if (maxValues[l] < acc[i][j][r])
                     {
                         for (int m = l; m < maxValues.size() - 1; m++)
                         {
                             maxValues[m+1] = maxValues[m];
                             maxCircles[m+1] = maxCircles[m];
                         }
-                        maxValues[l] = acc[i][j][k-rad_min];
-                        maxCircles[l] = { i, j, k };
+                        maxValues[l] = acc[i][j][r];
+                        maxCircles[l] = 
+                        {
+                            i * acc.step_i + acc.min_i,
+                            j * acc.step_j + acc.min_j,
+                            r * acc.step_r + acc.min_r,
+                        };
                         break;
                     }
                 }
